3629fcf769cbdfaa1a3e2c2a32366af37eb06b42
Vincze János <vinczit@gmail.com>
2021-03-01 16:41:58 +0100

    doc saved

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,44 @@
-'use strict';
+"use strict";
+const axios = require("axios");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+require("./db-connection");
+
+const Stock = mongoose.model(
+    "Stock",
+    new Schema({
+        stockData: {
+            stock: String,
+            price: Number,
+            likes: { type: Number, default: 0 },
+        },
+    })
+);
 
 module.exports = function (app) {
-
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    app.route("/api/stock-prices").get(function (req, res) {
+        //console.log(req.query);
+        let url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock}/quote`;
+        axios
+            .get(url)
+            .then(function (response) {
+                let stock = new Stock({
+                    stockData: {
+                        stock: req.query.stock,
+                        price: response.data.latestPrice,
+                        likes: 1,
+                    },
+                });
+                stock.save((err, doc) => {
+                    res.json({
+                        stock: doc.stockData.stock,
+                        price: doc.stockData.price,
+                    });
+                });
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
     });
-    
 };
---
+++ routes/db-connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const db = mongoose
+    .connect(process.env.DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        console.log("MongoDB connected...");
+    })
+    .catch((err) => console.log(err));
+
+module.exports = db;
