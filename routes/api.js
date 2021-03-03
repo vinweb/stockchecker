7ffd41a7cd76365a2f3319d639286306962a6b23
Vincze János <vinczit@gmail.com>
2021-03-03 18:02:54 +0100

    add likes

--- routes/api.js
+++ routes/api.js
@@ -22,23 +22,89 @@ module.exports = function (app) {
         axios
             .get(url)
             .then(function (response) {
-                let stock = new Stock({
+                // Ha nincs ilyen stock a felhőben:
+                if (response.data === "Unknown symbol") {
+                    return res.json({
+                        data: "Unknown symbol",
+                    });
+                }
+                let filter = {
+                    "stockData.stock": req.query.stock,
+                };
+                let update = () => {
+                    if (req.query.like) {
+                        return {
+                            $set: {
+                                "stockData.stock": req.query.stock.toUpperCase(),
+                                "stockData.price": response.data.latestPrice,
+                            },
+                            $inc: { "stockData.likes": 1 },
+                        };
+                    } else {
+                        return {
+                            $set: {
+                                "stockData.stock": req.query.stock.toUpperCase(),
+                                "stockData.price": response.data.latestPrice,
+                            },
+                        };
+                    }
+                };
+                let stockNoLike = new Stock({
                     stockData: {
-                        stock: req.query.stock,
+                        stock: req.query.stock.toUpperCase(),
+                        price: response.data.latestPrice,
+                    },
+                });
+                let stockWithLike = new Stock({
+                    stockData: {
+                        stock: req.query.stock.toUpperCase(),
                         price: response.data.latestPrice,
                         likes: 1,
                     },
                 });
-                stock.save((err, doc) => {
-                    res.json({
-                        stock: doc.stockData.stock,
-                        price: doc.stockData.price,
-                    });
-                });
+                Stock.findOneAndUpdate(
+                    filter,
+                    update(),
+                    { new: true },
+                    (err, updtatedDoc) => {
+                        if (err) return console.log(err);
+                        // Ha nincs ilyen stock a db-ben, mentse:
+                        if (!updtatedDoc && !req.query.like) {
+                            stockNoLike.save((err, doc) => {
+                                res.json({
+                                    stockData: {
+                                        stock: doc.stockData.stock,
+                                        price: doc.stockData.price,
+                                        likes: doc.stockData.likes,
+                                    },
+                                });
+                            });
+                        } else if (!updtatedDoc && req.query.like) {
+                            stockWithLike.save((err, doc) => {
+                                res.json({
+                                    stockData: {
+                                        stock: doc.stockData.stock,
+                                        price: doc.stockData.price,
+                                        likes: doc.stockData.likes,
+                                    },
+                                });
+                            });
+                        } else {
+                            // Ha van ilyen stock a db-ben, frissítette:
+                            res.json({
+                                stockData: {
+                                    stock: updtatedDoc.stockData.stock,
+                                    price: updtatedDoc.stockData.price,
+                                    likes: updtatedDoc.stockData.likes,
+                                },
+                            });
+                        }
+                    }
+                );
             })
             .catch(function (error) {
                 // handle error
-                console.log(error);
+                console.log("Ilyen nincs.");
             });
     });
 };
